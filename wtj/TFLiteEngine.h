5ab57bfdef120038ed61eac999262a8032b8255a
Jerin Philip <jerinphilip@live.in>
2024-04-03 00:03:53 +0530

    Apply clang-format

--- wtj/TFLiteEngine.h
+++ wtj/TFLiteEngine.h
@@ -5,19 +5,18 @@
 #include <vector>
 
 class TFLiteEngine {
-public:
-    TFLiteEngine() {};
-    ~TFLiteEngine() {};
+ public:
+  TFLiteEngine(){};
+  ~TFLiteEngine(){};
 
-    int loadModel(const char *modelPath, const bool isMultilingual);
-    void freeModel();
+  int loadModel(const char* modelPath, const bool isMultilingual);
+  void freeModel();
 
-    std::string transcribeBuffer(std::vector<float> samples);
-    std::string transcribeFile(const char* waveFile);
+  std::string transcribeBuffer(std::vector<float> samples);
+  std::string transcribeFile(const char* waveFile);
 
-private:
-    // Add any private members or helper functions as needed
+ private:
+  // Add any private members or helper functions as needed
 };
 
-#endif // _TFLITEENGINE_H_
-
+#endif  // _TFLITEENGINE_H_
