c8306959d1e779d2fbfeec1677949d26335348eb
Jerin Philip <jerinphilip@live.in>
2024-04-03 19:50:01 +0530

    More writeout refining

--- app/writeout.cpp
+++ app/writeout.cpp
@@ -19,10 +19,18 @@ int main(int argc, char **argv) {
   uint64_t filters_vocab_en_size = sizeof(filters_vocab_en);
   uint64_t filters_vocab_multilingual_size = sizeof(filters_vocab_multilingual);
   uint64_t tflt_vocab_mel_size = sizeof(tflt_vocab_mel_bin);
+  uint64_t input_features_size = sizeof(_content_input_features_bin);
 
   constexpr uint64_t kExpectedMelSize = 586174;
   if (tflt_vocab_mel_size != kExpectedMelSize) {
     fprintf(stderr, "Error. mismatch\n");
+  }
+
+  constexpr uint64_t kExpectedInputFeaturesSize = 960000;
+  if (input_features_size != kExpectedInputFeaturesSize) {
+    fprintf(stderr, "Error. mismatch %d != %d\n",
+            static_cast<int>(input_features_size),
+            static_cast<int>(kExpectedInputFeaturesSize));
   }
   // clang-format off
   printf("vocab-en size: %" PRIu64 "\n", filters_vocab_en_size);
@@ -44,6 +52,11 @@ int main(int argc, char **argv) {
     FILE *out = fopen(outpath.c_str(), "wb");
     fwrite(&tflt_vocab_mel_size, sizeof(uint64_t), 1, out);
     fwrite(tflt_vocab_mel_bin, tflt_vocab_mel_size, 1, out);
+    fclose(out);
+  } else if (asset == "input_features") {
+    FILE *out = fopen(outpath.c_str(), "wb");
+    fwrite(&input_features_size, sizeof(uint64_t), 1, out);
+    fwrite(_content_input_features_bin, input_features_size, 1, out);
     fclose(out);
   } else {
     FILE *out = fopen(outpath.c_str(), "wb");
