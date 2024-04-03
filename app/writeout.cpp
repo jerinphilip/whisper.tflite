7cf59f8863ef49fa48f5d1d7b963018b6a1d6065
Jerin Philip <jerinphilip@live.in>
2024-04-03 18:48:12 +0530

    Prepare a writeout app

---
+++ app/writeout.cpp
@@ -0,0 +1,6 @@
+#include "whisper.tflite/filters_vocab_en.h"
+#include "whisper.tflite/filters_vocab_multilingual.h"
+#include "whisper.tflite/input_features.h"
+#include "whisper.tflite/tflt-vocab-mel.h"
+
+int main(int argc, char **argv) { return 0; }
